b0cd6a5e14349c24aac0780512ff6fa53acae793
admin <admin@macpc2.esdoffice.local>
2015-03-04 18:08:14 +0200

    Table Header View Sample

--- TableHeaderView/AppDelegate.m
+++ TableHeaderView/AppDelegate.m
@@ -16,7 +16,17 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    self.navigationController = [[UINavigationController alloc]initWithRootViewController:self.viewController];
+    self.window.rootViewController = self.navigationController;
+    
+    
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
--- TableHeaderView/ViewController.m
+++ TableHeaderView/ViewController.m
@@ -16,12 +16,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.navigationItem.title = @"Table Header View";
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:0.9 green:0.4 blue:0.5 alpha:1.0];
+    self.navigationController.navigationBar.translucent = YES;
+    
+    UIView *headerView = [[[NSBundle mainBundle] loadNibNamed:@"HeaderView" owner:nil options:nil] firstObject];
+    //headerView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    self.tableView.tableHeaderView = headerView;
+    self.tableView.tableHeaderView.backgroundColor = [UIColor purpleColor];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 120;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"CellIdentifier";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        
+    }
+    
+    cell.backgroundColor = [UIColor lightGrayColor];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"Some text at row=%ld", (long)indexPath.row];
+    
+    return cell;
+}
+
 
 @end
